bdc7287dca1fd338f00e251bea10e1b13047615b
JoBenEtuk <JoBenEtuk@users.noreply.github.com>
2021-06-13 11:28:39 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <div className="app">
+      {/* Nav */}
+      <div className="nav false">
+        <img
+          className="nav__logo"
+          src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+          alt=""
+        />
+        <img
+          className="nav__avatar"
+          src="http://pngimg.com/uploads/netflix/netflix_PNG8.png"
+          alt=""
+        />
+      </div>
+      {/* End Nav */}
+      {/* Header */}
+      <header className="banner">
+        <div className="banner__contents">
+          <h1 className="banner__title">Ginny &amp; Georgia</h1>
+          <div className="banner__buttons">
+            <button className="banner__button">Play</button>
+            <button className="banner__button">My List</button>
+          </div>
+          <h1 className="banner__description">
+            Angsty and awkward fifteen year old Ginny Miller often feels more
+            mature than her thirty year old mother, the irresistible and dynamic
+            Georgia Miller...
+          </h1>
+        </div>
+        <div className="banner--fadeBottom"></div>
+      </header>
+      {/* Header */}
+    </div>
+  );
+}
+
+export default App;
