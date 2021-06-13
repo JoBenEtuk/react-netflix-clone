3e3336fea577b4950148619996a04aecfc678855
JoBenEtuk <JoBenEtuk@users.noreply.github.com>
2021-06-13 21:30:14 +0100

    responsive css

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,16 +1,22 @@
+import { Link } from "react-router-dom";
+
 function Nav() {
   return (
     <div className="nav false">
-      <img
-        className="nav__logo"
-        src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
-        alt=""
-      />
-      <img
-        className="nav__avatar"
-        src="http://pngimg.com/uploads/netflix/netflix_PNG8.png"
-        alt=""
-      />
+      <Link to="/">
+        <img
+          className="nav__logo"
+          src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+          alt=""
+        />
+      </Link>
+      <Link to="/">
+        <img
+          className="nav__avatar"
+          src="http://pngimg.com/uploads/netflix/netflix_PNG8.png"
+          alt=""
+        />
+      </Link>
     </div>
   );
 }
